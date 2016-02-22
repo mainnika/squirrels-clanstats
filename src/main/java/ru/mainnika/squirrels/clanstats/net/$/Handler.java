bc6da5bc2a50686e39cf576e5574fc6677352b90
mainnika <nikita@tokarch.uk>
2016-02-22 19:36:33 +0500

    temporary remove core files

--- src/main/java/ru/mainnika/squirrels/clanstats/net/$/Handler.java
+++
@@ -1,9 +0,0 @@
-package ru.mainnika.squirrels.clanstats.net.$;
-
-import ru.mainnika.squirrels.clanstats.net.Packet;
-import ru.mainnika.squirrels.clanstats.net.Receiver;
-
-public interface Handler
-{
-	void handle(Receiver receiver, Packet packet) throws Exception;
-}
